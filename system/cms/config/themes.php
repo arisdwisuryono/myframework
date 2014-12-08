e18670597df36b067c5cf76cdfb6325bd8ab19fa
aris.dwi.suryono <aris.dwi.suryono@gmail.com>
2014-12-08 13:03:33 +0700

    adding asset, template, themes config

---
+++ system/cms/config/themes.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+// Theme
+$config['theme']				= 'sb-admin';
+// without trailing slash "/"
+$config['theme_folder']			= 'assets/themes';
+
+
+
+$config['assets_folder']			= 'assets';
+
+
